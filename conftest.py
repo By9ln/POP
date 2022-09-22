4a5cc0b739fa20be5e667b7c2e1d34c6593e9c7c
Vladimir Lyanoy <By9lnEs@requestum.com>
2022-09-22 23:02:35 +0300

    add tests

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+options = Options()
+options.add_experimental_option('prefs', {'intl.accept_languages': 'en'})
+browser = webdriver.Chrome(options=options)
